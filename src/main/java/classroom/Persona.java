e861cc2a3de0b190a0679fe304e104ddbd91eed3
santix-l <137665289+santix-l@users.noreply.github.com>
2023-09-02 13:53:42 -0500

    Update Persona.java

--- src/main/java/classroom/Persona.java
+++ src/main/java/classroom/Persona.java
@@ -24,7 +24,7 @@ public class Persona {
 
     public Persona(long cedula) {
         this.cedula = cedula;
-        this.nombre = "Sin nombre";
+        //this.nombre = "Sin nombre";
         totalPersonas++;
     }
 
